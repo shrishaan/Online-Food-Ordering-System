14c675ba85a463d9ff6db61770d1a2f122eb20e1
Shrishaan Bajracharya <shreetheshan@gmail.com>
2025-09-24 19:02:26 +0545

    Started Payment Interface

---
+++ User/Payment.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Foodie.User
+{
+    public partial class Payment : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr, dr1;
+        DataTable dt;
+        SqlTransaction transcation = null;
+        string _name = string.Empty; string _cardNo = string.Empty; string _expiryDate = string.Empty; string _cvv = string.Empty; string _address = string.Empty; string _paymentMode = string.Empty;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["userId"] == null)
+                {
+                    Response.Redirect("Login.aspx");
+                }
+            }
+        }
+
+
+        //Card Payment
+        protected void lbCardSubmit_Click(object sender, EventArgs e)
+        {
+            _name = txtName.Text.Trim();
+            _cardNo = txtCardNo.Text.Trim();
+            _cardNo = string.Format("************{0}", txtCardNo.Text.Trim().Substring(12, 4)); //starting 12 chac will be hashed and rest 4 will be as it is.
+            _expiryDate = txtExpMonth.Text.Trim() + "/" + txtExpYear.Text.Trim(); // Will save in MM/YY  format
+            _cvv = txtCvv.Text.Trim();
+            _address = txtAddress.Text.Trim();
+            _paymentMode = "card";
+            if (Session["userId"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
+
+        }
+
+        //Cash on delivery
+        protected void lbCodSubmit_Click(object sender, EventArgs e)
+        {
+            _address = txtCODAddress.Text.Trim();
+            _paymentMode = "cod";
+            if (Session["userId"] != null)
+            {
+
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+        void OrderPayment(string name, string cardNo, string expiryDate, string cvv, string address, string paymentMode)
+        {
+            int paymentId; int productId; int quantity;
+            dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[7]
+            {
+                new DataColumn("OrderNo", typeof(string)),
+                new DataColumn("ProductId", typeof(int)),
+                new DataColumn("Quantity", typeof(int)),
+                new DataColumn("UserId", typeof(int)),
+                new DataColumn("Status", typeof(string)),
+                new DataColumn("PaymentId", typeof(int)),
+                new DataColumn("OrderDate", typeof(DateTime)),
+            });
+
+            con = new SqlConnection(Connection.GetConnectionString());
+            con.Open();
+            #region Sql Transaction
+            transcation = con.BeginTransaction(); // only if connection is Open it will work
+            cmd = new SqlCommand("Save_Payment", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Name", name);
+            cmd.Parameters.AddWithValue("@CardNo", cardNo);
+            cmd.Parameters.AddWithValue("@ExpiryDate", expiryDate);
+            cmd.Parameters.AddWithValue("@Cvv", cvv);
+            cmd.Parameters.AddWithValue("@Address", address);
+            cmd.Parameters.AddWithValue("@PaymentMode", paymentMode);
+            cmd.Parameters.Add("@InsertedId", SqlDbType.Int);
+            cmd.Parameters["@InsertedId"].Direction = ParameterDirection.Output;
+            try
+            {
+                cmd.ExecuteNonQuery();
+                paymentId = Convert.ToInt32(cmd.Parameters["@InsertedId"].Value);
+
+                #region Getting Cart Item's
+                cmd = new SqlCommand("Cart_Crud", con);
+                cmd.Parameters.AddWithValue("@Action", "SELECT");
+                cmd.Parameters.AddWithValue("@UserId", Session["userId"]);
+                cmd.CommandType = CommandType.StoredProcedure;
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    productId = (int)dr["ProductId"];
+                    quantity = (int)dr["Quantity"];
+                    // Update The Product Quantity
+
+                    //Delete Cart Item
+                }
+                dr.Close();
+                #endregion Getting Cart Items
+            }
+            catch (Exception e)
+            {
+                try
+                {
+                    transcation.Rollback();
+                }
+                catch (Exception ex) 
+                {
+                    Response.Write("<script>alert('Error: " + ex.Message + "');</script>");
+                }
+            }
+            #endregion Sql Transaction
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
--- User/Payment.aspx.designer.cs
+++ User/Payment.aspx.designer.cs
@@ -11,7 +11,7 @@ namespace Foodie.User
 {
 
 
-    public partial class Login
+    public partial class Payment
     {
 
         /// <summary>
